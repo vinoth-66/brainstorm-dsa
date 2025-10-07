bf16386e7e34219a16e3f0ec4590678085e2c525
VINOTH M <vinothtechstack@gmail.com>
2025-10-07 14:13:54 +0530

    Time: 0 ms (100%), Space: 40.5 MB (59.13%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int integerReplacement(int n) {
+        int c=0;
+        long N=n;
+        while(N>1){
+            if(N%2==0) N/=2;
+            else if(N==3 || N%4==1) N-=1;
+            else N+=1;
+            c++;
+        }
+        return c;
+    }
+}
