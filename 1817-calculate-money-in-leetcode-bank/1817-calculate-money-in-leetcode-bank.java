ce1ce5285eb94ced4f3f4762ead2c99e428f719f
VINOTH M <vinothtechstack@gmail.com>
2025-10-25 18:56:49 +0530

    Time: 1 ms (56.85%), Space: 40.9 MB (15.3%) - LeetHub

---
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int totalMoney(int n) {
+        int ans=0;
+        int s=1;
+        int e=7;
+        int in=1;
+        for(int i=0;i<n;i++){
+            if(in==e){
+                s+=1;
+                in=s;
+                ans+=e;
+                e+=1;
+            }
+            else{
+                ans+=in;
+                in+=1;
+            }
+        }
+        return ans;
+    }
+}
