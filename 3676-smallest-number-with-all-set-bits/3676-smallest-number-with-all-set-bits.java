e212b11ff806e265a48bde740d01c4d281f1d4be
VINOTH M <vinothtechstack@gmail.com>
2025-10-29 18:27:12 +0530

    Time: 0 ms (100%), Space: 41 MB (36.78%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
@@ -1,10 +1,14 @@
 class Solution {
     public int smallestNumber(int n) {
-        while(true){
-            String s=Integer.toBinaryString(n++);
-            if(s.contains("0")!=true){
-                return Integer.parseInt(s,2);
-            }
-        }
+        // while(true){
+        //     String s=Integer.toBinaryString(n++);
+        //     if(s.contains("0")!=true){
+        //         return Integer.parseInt(s,2);
+        //     }
+        // }
+
+        int x=n;
+        while((x&(x+1))!=0) x++;
+        return x;
     }
 }
