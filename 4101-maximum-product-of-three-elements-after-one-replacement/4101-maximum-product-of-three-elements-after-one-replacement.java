834d6f72fe84b3163fbfbb3513c5591b8d3efc1d
VINOTH M <vinothtechstack@gmail.com>
2025-11-07 13:29:56 +0530

    Time: 43 ms (50.76%), Space: 96.5 MB (87.81%) - LeetHub

---
+++ 4101-maximum-product-of-three-elements-after-one-replacement/4101-maximum-product-of-three-elements-after-one-replacement.java
@@ -0,0 +1,47 @@
+class Solution {
+    public long maxProduct(int[] nums) {
+        long ans=1;
+        Arrays.sort(nums);
+        int c=0;
+        if(nums[0]<0 && nums[nums.length-1]<0) c=-1;
+        else if(nums[0]>0 && nums[nums.length-1]>0) c=1;
+        else c=0;
+        if(c==1) ans=pos(ans,nums);
+        else if(c==0) ans=both(ans,nums);
+        else ans=neg(ans,nums);
+        return ans;
+    }
+    private long pos(long ans,int[] nums){
+        int n=nums.length-1;
+        for(int i=0;i<2;i++){
+            ans*=(long)nums[n--];
+        }
+        ans*=100000L;
+        return ans;
+    }
+    private long neg(long ans,int[] nums){
+        for(int i=0;i<2;i++){
+            ans*=(long)nums[i];
+        }
+        ans*=100000L;
+        return ans;
+    }
+    private long both(long ans,int[] nums){
+        int s=0,e=nums.length-1;
+        int c=0;
+        for(int i=0;i<2;i++){
+            if(Math.abs(nums[s])>nums[e]){
+                ans*=(long)nums[s];
+                c++;
+                s++;
+            }
+            else{
+                ans*=(long)nums[e];
+                e--;
+            }
+        }
+        if(c%2==0) ans*=100000L;
+        else ans*=-100000L;
+        return ans;
+    }
+}
