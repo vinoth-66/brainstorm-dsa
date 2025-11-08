b64330782b700c2235cdba02e40c421c6cf3fa36
VINOTH M <vinothtechstack@gmail.com>
2025-11-08 10:31:17 +0530

    Time: 67 ms (38.12%), Space: 191.9 MB (21.29%) - LeetHub

---
+++ 4078-maximum-alternating-sum-of-squares/4078-maximum-alternating-sum-of-squares.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        vector<int> pos(nums.size());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<0) pos[i]=abs(nums[i]);
+            else pos[i]=nums[i];
+        }
+        sort(pos.begin(),pos.end());
+        vector<int> ans(nums.size());
+        int s=0,e=nums.size()-1;
+        int in=0;
+        while(s<=e){
+            if(in%2==0) ans[in++]=pos[e--];
+            else ans[in++]=pos[s++];
+        }
+        long sum=0;
+        for(int i=0;i<ans.size();i++){
+            if(i%2==0) sum+=ans[i]*ans[i];
+            else sum-=ans[i]*ans[i];
+        }
+        return sum;
+    }
+};
