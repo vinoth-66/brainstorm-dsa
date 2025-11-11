ef0381dd2f4aef8fb0dcfb89fb7f8f96707565c8
VINOTH M <vinothtechstack@gmail.com>
2025-11-11 13:07:43 +0530

    Time: 0 ms (100%), Space: 44.1 MB (5.45%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int c=0;
+        for(int i:arr){
+            if(i%2!=0){
+                c++;
+                if(c==3) return true;
+            }
+            else{
+                c=0;
+            }
+        }
+        return false;
+    }
+}
