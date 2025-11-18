82b86c6be6f5db7ef29b53be5d0466d95801dbe4
VINOTH M <vinothtechstack@gmail.com>
2025-11-19 00:29:45 +0530

    Time: 0 ms (100%), Space: 31 MB (87.04%) - LeetHub

---
+++ 4112-maximize-expression-of-three-elements/4112-maximize-expression-of-three-elements.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int maximizeExpressionOfThree(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()-2]+nums[nums.size()-1]-nums[0];
+    }
+};
