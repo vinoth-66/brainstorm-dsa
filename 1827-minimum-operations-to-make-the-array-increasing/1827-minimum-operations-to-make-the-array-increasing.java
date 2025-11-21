fd74e94e6e2f510f41deda89e97f84356cf2a3c5
VINOTH M <vinothtechstack@gmail.com>
2025-11-21 22:23:37 +0530

    Time: 4 ms (76.09%), Space: 46.7 MB (24.25%) - LeetHub

---
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int ans=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>=nums[i+1]){
+                ans+=nums[i]+1-nums[i+1];
+                nums[i+1]=nums[i]+1;
+            }
+        }
+        return ans;
+    }
+}
