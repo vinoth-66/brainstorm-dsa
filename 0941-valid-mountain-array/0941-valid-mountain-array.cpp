f70fe7bcbbcb08b18cb2e8ee3c4c8b81b2b11d48
VINOTH M <vinothtechstack@gmail.com>
2025-11-27 23:29:17 +0530

    Time: 19 ms (66.31%), Space: 26.2 MB (72.68%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        if(arr.size()<3) return false;
+        int i=0,j=arr.size()-1;
+        while(i<j && arr[i]<arr[i+1]) i++;
+        while(j>0 && arr[j]<arr[j-1]) j--;
+        return i==j && i!=0 && i!=arr.size()-1;
+    }
+};
