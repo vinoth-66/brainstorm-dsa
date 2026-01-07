c5775e0337486e20523ddaccd25eee0c81711cab
VINOTH M <vinothtechstack@gmail.com>
2026-01-07 18:36:54 +0530

    Time: 129 ms (7.49%), Space: 72.7 MB (30.5%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,18 +1,20 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int tar=0;
-        for(int i:nums) tar+=i;
-        if(tar%2!=0) return false;
-        Boolean[][] memo=new Boolean[nums.length][tar/2+1];
-        return subset(nums,0,tar/2,memo);
+        int s=0;
+        for(int i:nums) s+=i;
+        if(s%2!=0) return false;
+        Boolean[][] memo=new Boolean[nums.length][s/2+1];
+        for(Boolean i[]:memo) Arrays.fill(i,null);
+        return fun(nums,0,s/2,memo);
     }
-    private boolean subset(int[] nums,int in,int tar,Boolean[][] memo){
-        if(tar==0) return true;
-        if(tar<0) return false;
-        if(in>nums.length-1) return false;
-        if(memo[in][tar]!=null) return memo[in][tar];
-        boolean pick=subset(nums,in+1,tar-nums[in],memo);
-        boolean leave=subset(nums,in+1,tar,memo);
-        return memo[in][tar]=pick||leave;
+    private boolean fun(int[] nums,int in,int s,Boolean[][] memo){
+        if(s==0) return true;
+        else if(s<0) return false;
+        if(in==nums.length) return false;
+        if(memo[in][s]!=null) return memo[in][s]; 
+        boolean p=fun(nums,in+1,s-nums[in],memo);
+        boolean l=fun(nums,in+1,s,memo);
+        memo[in][s]=p||l;
+        return memo[in][s];
     }
 }
