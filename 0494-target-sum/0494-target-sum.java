89f8fdde14dd652ca045844c3a07819d9dae9487
VINOTH M <vinothtechstack@gmail.com>
2026-01-07 18:11:39 +0530

    Time: 8 ms (59.76%), Space: 46.8 MB (26.06%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,15 +1,22 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int[] ans=new int[1];
-        fun(nums,0,target,0,ans);
-        return ans[0];
+        int ts = 0;
+        for (int num : nums) ts += num;
+        if(ts<Math.abs(target)) return 0;
+        int[][] memo=new int[nums.length][2*ts+1];
+        for(int[] i:memo) Arrays.fill(i,Integer.MIN_VALUE);
+        return fun(nums,0,target,0,ts,memo);
     }
-    private void fun(int[] nums,int in,int t,int s,int[] ans){
+    private int fun(int[] nums,int in,int t,int s,int ts,int[][] memo){
         if(in==nums.length){
-            if(t==s) ans[0]+=1;
-            return;
+            if(t==s) return 1;
+            return 0;
         }
-        fun(nums,in+1,t,s-nums[in],ans);
-        fun(nums,in+1,t,s+nums[in],ans);
+        int dynamicSum=ts+s;
+        if(memo[in][dynamicSum]!=Integer.MIN_VALUE) return memo[in][dynamicSum];
+        int a=fun(nums,in+1,t,s-nums[in],ts,memo);
+        int b=fun(nums,in+1,t,s+nums[in],ts,memo);
+        memo[in][dynamicSum]=a+b;
+        return memo[in][dynamicSum];
     }
 }
