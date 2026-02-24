18e26ddf144c1d52b79b10c0cfbc4ebd9daa9bbb
VINOTH M <vinothtechstack@gmail.com>
2026-02-24 18:19:24 +0530

    Time: 0 ms (100%), Space: 44.1 MB (28.76%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -16,17 +16,16 @@
 class Solution {
     public int sumRootToLeaf(TreeNode root) {
         // int[] ans=new int[1];
-        return vin(root,"");
+        return vin(root,0);
         // return ans[0];
     }
-    private int vin(TreeNode root,String t){
+    private int vin(TreeNode root,int t){
         if(root==null){
             return 0;
         }
-        t+=root.val;
+        t = t * 2 + root.val;
         if(root.left==null && root.right==null){
-            int v=Integer.parseInt(t,2);
-            return v;
+            return t;
         }
         int l=vin(root.left,t);
         int r=vin(root.right,t);
