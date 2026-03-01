3c435648912215c899c9a56605868a36d127584b
VINOTH M <vinothtechstack@gmail.com>
2026-03-01 12:40:03 +0530

    Time: 1 ms (20.57%), Space: 42.7 MB (18.34%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.java
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.java
@@ -1,10 +1,12 @@
 class Solution {
     public int minCost(int n) {
-        return vin(n);
+        int[] memo=new int[n+1];
+        Arrays.fill(memo,-1);
+        return vin(n,memo);
     }
-    private int vin(int n){
+    private int vin(int n,int[] memo){
         if(n==1) return 0;
-
+        if(memo[n]!=-1) return memo[n];
         int l=0,r=0;
         if(n%2==0){
             l=n/2;
@@ -14,9 +16,9 @@ class Solution {
             l=n/2;
             r=l+1;
         }
-        int left=vin(l);
-        int right=vin(r);
+        int left=vin(l,memo);
+        int right=vin(r,memo);
 
-        return (l*r) + (left) + (right);
+        return memo[n]=(l*r) + (left) + (right);
     }
 }
