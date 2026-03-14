36525d05b354acb5f67a9c891f8a4de43aba9387
VINOTH M <vinothtechstack@gmail.com>
2026-03-14 23:31:41 +0530

    Time: 61 ms (67.75%), Space: 107.6 MB (43.75%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,22 +1,33 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        // int[] ans=new int[temperatures.length];
+        // Stack<int[]> st=new Stack<>();
+        // for(int i=0;i<temperatures.length;i++){
+        //     if(st.isEmpty()) st.push(new int[]{temperatures[i],i});
+        //     else{
+        //         int[] v=st.peek();
+        //         if(v[0]>temperatures[i]){
+        //             st.push(new int[]{temperatures[i],i});
+        //         }
+        //         else{
+        //             while(!st.isEmpty() && st.peek()[0]<temperatures[i]){
+        //                 ans[st.peek()[1]]=i-st.peek()[1];
+        //                 st.pop();
+        //             }
+        //             st.push(new int[]{temperatures[i],i});
+        //         }
+        //     }
+        // }
+        // return ans;
+
         int[] ans=new int[temperatures.length];
-        Stack<int[]> st=new Stack<>();
+        Stack<Integer> st=new Stack<>();
         for(int i=0;i<temperatures.length;i++){
-            if(st.isEmpty()) st.push(new int[]{temperatures[i],i});
-            else{
-                int[] v=st.peek();
-                if(v[0]>temperatures[i]){
-                    st.push(new int[]{temperatures[i],i});
-                }
-                else{
-                    while(!st.isEmpty() && st.peek()[0]<temperatures[i]){
-                        ans[st.peek()[1]]=i-st.peek()[1];
-                        st.pop();
-                    }
-                    st.push(new int[]{temperatures[i],i});
-                }
+            while(!st.isEmpty() && temperatures[st.peek()]<temperatures[i]){
+                int in=st.pop();
+                ans[in]=i-in;
             }
+            st.push(i);
         }
         return ans;
     }   
