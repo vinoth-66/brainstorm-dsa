719ff001dbd08831111b027d42b57ed60b38f257
VINOTH M <vinothtechstack@gmail.com>
2026-03-14 09:35:09 +0530

    Time: 12 ms (41.98%), Space: 46.8 MB (36.64%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -2,19 +2,21 @@ class Solution {
     public String getHappyString(int n, int k) {
         List<String> ans=new ArrayList<>();
         String str="abc";
-        vin(n,-1,str,"",ans);
+        vin(n,-1,str,new StringBuilder(),ans);
         if(ans.size()<k) return "";
         return ans.get(k-1);
     }
-    private void vin(int n,int in,String str,String s,List<String> ans){
-        if(s.length()==n){
-            ans.add(s);
+    private void vin(int n,int in,String str,StringBuilder sb,List<String> ans){
+        if(sb.length()==n){
+            ans.add(sb.toString());
             return;
         }
 
         for(int i=0;i<str.length();i++){
             if(i==in) continue;
-            vin(n,i,str,s+str.charAt(i),ans);
+            sb.append(str.charAt(i));
+            vin(n,i,str,sb,ans);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
 }
