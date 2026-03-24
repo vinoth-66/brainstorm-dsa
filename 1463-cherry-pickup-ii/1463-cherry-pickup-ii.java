b7e37db11f624a3445d66909831b7ca3695da486
VINOTH M <vinothtechstack@gmail.com>
2026-03-24 21:29:57 +0530

    Time: 26 ms (54.41%), Space: 46.3 MB (83.54%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int cherryPickup(int[][] grid) {
+        int r=grid.length;
+        int c=grid[0].length;
+        int[][][] memo=new int[r][c][c];
+        for(int[][] i:memo){
+            for(int[] j:i) Arrays.fill(j,-1);
+        }
+        return vin(r,c,0,0,c-1,grid,memo);
+    }
+    private int vin(int r,int c,int i,int j1,int j2,int[][] mat,int[][][] memo){
+        if(j1<0 || j1>c-1 || j2<0 || j2>c-1) return -(int)1e9;
+        else if(i==r-1){
+            if(j1==j2) return mat[i][j1];
+            else return mat[i][j1]+mat[i][j2];
+        }
+        if(memo[i][j1][j2]!=-1) return memo[i][j1][j2];
+        int max=-1;
+        for(int di=-1;di<=1;di++){
+            for(int dj=-1;dj<=1;dj++){
+                int v=0;
+                if(j1==j2) v=mat[i][j1];
+                else v=mat[i][j1]+mat[i][j2];
+                v+=vin(r,c,i+1,j1+di,j2+dj,mat,memo);
+                max=Math.max(max,v);
+            }
+        }
+        return memo[i][j1][j2]=max;
+    }
+}
