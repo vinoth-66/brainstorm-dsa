b278fdd1e7ff174640e66c681192fbe6a7069f50
VINOTH M <vinothtechstack@gmail.com>
2026-04-07 18:47:50 +0530

    Time: 242 ms (5.37%), Space: 164.3 MB (76.86%) - LeetHub

---
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> findWinners(int[][] matches) {
+        TreeMap<Integer,Integer> map=new TreeMap<>();
+        int r=matches.length;
+        int v=0;
+        for(int i=0;i<r;i++){
+            v=matches[i][1];
+            map.put(v,map.getOrDefault(v,0)+1);
+        }
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> in2=new ArrayList<>();
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<r;i++){
+            if(!map.containsKey(matches[i][0])) set.add(matches[i][0]);
+        }
+        List<Integer> in1=new ArrayList<>(set);
+        Collections.sort(in1);
+        ans.add(in1);
+        for(Map.Entry<Integer,Integer> m:map.entrySet()){
+            if(m.getValue()==1) in2.add(m.getKey());
+        }
+        ans.add(in2);
+        return ans;
+    }
+}
