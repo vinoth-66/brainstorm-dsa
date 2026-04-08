dbf29c484c578eb203b016a193631a3ee420da4a
VINOTH M <vinothtechstack@gmail.com>
2026-04-08 07:04:36 +0530

    Time: 67 ms (16%), Space: 47.8 MB (69.33%) - LeetHub

--- 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.java
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.java
@@ -3,7 +3,7 @@ class Solution {
         int mod=1000000007;
         for(int[] i:queries){
             for(int j=i[0];j<=i[1];j+=i[2]){
-                nums[j]=(int)((1L*nums[j]*i[3])%mod);
+                nums[j]=(int)(((long)nums[j]*i[3])%mod);
             }
         }
         long ans=nums[0];;
