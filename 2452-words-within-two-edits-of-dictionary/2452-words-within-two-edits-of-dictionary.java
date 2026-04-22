65471c8b33507d0accaeacbfed0c813e60bb625f
VINOTH M <vinothtechstack@gmail.com>
2026-04-22 07:48:38 +0530

    Time: 3 ms (83.09%), Space: 44.1 MB (97.06%) - LeetHub

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        Set<String> set=new HashSet<>();
+        for(String s: queries){
+            set.add(s);
+        }
+        Set<String> ans=new HashSet<>();
+        for(String s:dictionary){
+            if(set.contains(s)) ans.add(s);
+        }
+        for(String s:queries){
+            if(ans.contains(s)) continue;
+            for(String str:dictionary){
+                if(isValid(s,str)) ans.add(s);
+            }
+        }
+        List<String> res=new ArrayList<>();
+        for(String s:queries){
+            if(ans.contains(s)) res.add(s);
+        }
+        return res;
+    }
+    public static boolean isValid(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        int diff = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+                if (diff > 2) return false;
+            }
+        }
+        return true;
+    }
+}
