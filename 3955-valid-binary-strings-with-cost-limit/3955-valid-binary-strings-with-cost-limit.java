a8734947cff54d664e73308ea03b361f9884669b
VINOTH M <vinothtechstack@gmail.com>
2026-06-08 21:49:47 +0530

    Time: 10 ms (100%), Space: 47.5 MB (100%) - LeetHub

--- 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.java
+++ 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.java
@@ -1,28 +1,29 @@
 class Solution {
     public List<String> generateValidStrings(int n, int k) {
         List<String> ans=new ArrayList<>();
-        vin(n,k,new StringBuilder(),0,ans);
+        vin(n,k,new StringBuilder(),0,ans,0,0);
         return ans;
     }
-    public void vin(int n,int k,StringBuilder sb,int f,List<String> ans){
+    public void vin(int n,int k,StringBuilder sb,int f,List<String> ans,int cost,int ind){
         if(sb.length()==n){
-            if(helper(sb,k)) ans.add(sb.toString());
+            if(cost<=k) ans.add(sb.toString());
+            // if(helper(sb,k)) ans.add(sb.toString());
             return;
         }
         if(f!=1){
             sb.append("1");
-            vin(n,k,sb,1,ans);
+            vin(n,k,sb,1,ans,cost+ind,ind+1);
             sb.deleteCharAt(sb.length()-1);
         }
         sb.append("0");
-        vin(n,k,sb,0,ans);
+        vin(n,k,sb,0,ans,cost,ind+1);
         sb.deleteCharAt(sb.length()-1);
     }
-    public boolean helper(StringBuilder sb,int k){
-        int s=0;
-        for(int i=0;i<sb.length();i++){
-            if(sb.charAt(i)=='1') s+=i;
-        }
-        return s<=k;
-    } 
+    // public boolean helper(StringBuilder sb,int k){
+    //     int s=0;
+    //     for(int i=0;i<sb.length();i++){
+    //         if(sb.charAt(i)=='1') s+=i;
+    //     }
+    //     return s<=k;
+    // } 
 }
