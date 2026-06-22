3a4987413b1c5a6573122a577671fa8268fe9edd
VINOTH M <vinothtechstack@gmail.com>
2026-06-22 18:03:51 +0530

    Time: 37 ms (56.96%), Space: 84.3 MB (8.8%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int c=0;
+        for(int i=0;i<costs.length;i++){
+            if(coins-costs[i]>=0){
+                coins-=costs[i];
+                c++;
+            }
+            else break;
+        }
+        return c;
+    }
+}
