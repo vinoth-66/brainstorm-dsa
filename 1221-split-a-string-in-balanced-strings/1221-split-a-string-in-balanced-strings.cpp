a668f71a2fa81df3902b158f6a08594b8c93e235
VINOTH M <vinothtechstack@gmail.com>
2026-08-21 22:55:34 +0530

    Time: 2 ms (3.12%), Space: 8.3 MB (84.55%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int l=0,r=0;
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='L') l++;
+            else r++;
+            if(l==r){
+                ans++;
+                l=0;
+                r=0;
+            }
+        }
+        return ans;
+    }
+};
